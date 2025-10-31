a1c10f070260a3ed89f0c5ff46e0c38b03fa1cbb
GB <xorbskim6105@gmail.com>
2025-10-31 12:05:28 +0900

    feat($Parser) : 파서 기능 추가
    콘솔로 입력 받은 문자를 필요한 자료형으로 변환하는 파서를 추가
    Parser 추가
    price - 금액 변환  메서드
    targetNumber - 당첨 번호 변환 메서드
    bonusNumber - 보너스 번호 볂환 메서드
    getPurchaseAmount - 금액 -> 구매 개수 변환 메서드

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -1,2 +1,44 @@
-package lotto.util;public class Parser {
+package lotto.util;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+    private static final int PRICE_UNIT = 1000;
+
+    // 입력 받은 금액을 정수로 반환
+    public int price(String price) {
+        try {
+            return Integer.parseInt(price);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // 입력 받은 당첨번호를 정수 List로 반환
+    public List<Integer> targetNumber(String number) {
+        try {
+            return Arrays.stream(number.split(","))
+                    .map(String::strip)
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    // 입력 받은 보너스 번호를 정수로 반환
+    public int bonusNumber(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getPurchaseAmount(int price){
+        return price / PRICE_UNIT;
+    }
 }
