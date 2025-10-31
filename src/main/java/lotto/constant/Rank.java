e7f94b4d51ae078ed788fcba99a2718983b6df61
GB <xorbskim6105@gmail.com>
2025-10-31 14:59:31 +0900

    feat($Rank) : rank 추가
    매칭 개수에 따른 순위, 상금, 개수 정보 를 가지는 ENUM 추가

--- src/main/java/lotto/constant/Rank.java
+++ src/main/java/lotto/constant/Rank.java
@@ -1,2 +1,42 @@
-package lotto.constant;public enum Rank {
+package lotto.constant;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(1, false, 2000000000, 6),
+    SECOND(2, true, 30000000, 5),
+    THIRD(3, false, 1500000, 5),
+    FOURTH(4, false, 50000,4),
+    FIFTH(5, false, 5000, 3),
+    NONE(0,false,0,0);
+
+    Rank(int rank, boolean isBonus, int prize, int matchingCount){
+        this.rank = rank;
+        this.isBonus = isBonus;
+        this.prize = prize;
+        this.matchingCount = matchingCount;
+    }
+    private final int rank;
+    private final boolean isBonus;
+    private final int prize;
+    private final int matchingCount;
+
+    public static Rank of(int matchingCount, boolean hasBonus){
+        if(matchingCount < 3){
+            return NONE;
+        }
+
+        return Arrays.stream(values())
+                .filter(rank -> rank != NONE)
+                .filter(rank -> rank.matchingCount == matchingCount)
+                .filter(rank -> {
+                    if(matchingCount == 5){
+                        return rank.isBonus == hasBonus;
+                    }
+                    return true;
+                })
+                .findFirst()
+                .orElse(NONE);
+    }
+
 }
