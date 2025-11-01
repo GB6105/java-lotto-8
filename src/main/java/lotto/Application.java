669b230a720f9b1870076d8eb8652ea4817748e8
GB <xorbskim6105@gmail.com>
2025-11-01 21:40:47 +0900

    feat($LottoController) : 실행 계층 추가
    프로그램을 실행하는 계층을 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import lotto.application.InputController;
+import lotto.application.LottoController;
+import lotto.application.LottoService;
+import lotto.domain.LottoGenerator;
+import lotto.util.Parser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Parser parser = new Parser();
+        InputController inputController = new InputController(inputView,outputView,parser);
+        LottoGenerator generator = new LottoGenerator();
+        LottoService lottoService = new LottoService(generator);
+        LottoController controller = new LottoController(inputController, outputView, lottoService);
+
+        controller.run();
+
     }
 }
--- src/main/java/lotto/application/LottoController.java
+++ src/main/java/lotto/application/LottoController.java
@@ -1,34 +1,48 @@
-package lotto;
+package lotto.application;
 
 import java.util.List;
-import lotto.domain.Checker;
-import lotto.domain.RandomNumber;
-import lotto.domain.RandomNumbers;
+import java.util.Map;
+import lotto.constant.Rank;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.LottoResult;
+import lotto.domain.Lottos;
+import lotto.domain.WinningNumbers;
 import lotto.util.Parser;
 import lotto.util.Validator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
-    public void run(){
-        InputView inputView = new InputView();
-        Parser parser = new Parser();
+    private InputController inputController;
+    private OutputView outputView;
+    private LottoService lottoService;
 
-        String priceInput = inputView.readPrice();
-        int price = parser.price(priceInput);
-        Validator.validatePriceRange(price);
-        int amount = parser.getPurchaseAmount(price);
-
-        String targetNumberInput = inputView.readNumbers();
-        List<Integer> targetNumber = parser.targetNumber(targetNumberInput);
-        Validator.validateNumbersRange(targetNumber);
-
-        String bonusNumber = inputView.readBonusNumber();
-        int bonusNumberInt = parser.bonusNumber(bonusNumber);
-        Validator.validateNumberRange(bonusNumberInt);
-
-        RandomNumbers randomNumbers = new RandomNumbers(amount);
-        Checker checker = new Checker(randomNumbers,targetNumber,bonusNumberInt);
-
-
+    public LottoController(InputController inputController, OutputView outputView, LottoService lottoService) {
+        this.inputController = inputController;
+        this.outputView = outputView;
+        this.lottoService = lottoService;
     }
+
+    public void run() {
+        // 구매 금액 입력
+        int price = inputController.getPrice();
+
+        //로또 구입
+        Lottos lottos = lottoService.purchaseLottos(price);
+        outputView.printPurchaseMessage(price / 1000);
+        outputView.printLottos(lottos);
+
+        // 당첨 번호 입력
+        List<Integer> winningNumber = inputController.getWinningNumber();
+
+        // 보너스 번호 입력
+        int bonusNumber = inputController.getBonusNumber();
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumber, bonusNumber);
+
+        //결과 출력
+        LottoResult result = lottoService.checkLottos(lottos,winningNumbers,price);
+        outputView.printTotalResult(result);
+    }
+
 }
