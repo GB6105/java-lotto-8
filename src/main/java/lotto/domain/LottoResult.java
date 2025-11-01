3a004ba345206b6aa9cd281edf1106ad95ca8023
GB <xorbskim6105@gmail.com>
2025-11-01 21:32:50 +0900

    feat($LottoResult) : 당첨 결과 관리 기능 추가
    당첨 결과와 수익률을 관리하는 기능 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,2 +1,49 @@
-package lotto.domain;public class LottoResult {
+package lotto.domain;
+
+import java.util.Map;
+import lotto.constant.Rank;
+
+public class LottoResult {
+    private final Map<Rank, Integer> matchCounts;
+    private final double profitRate;
+
+    public LottoResult(Map<Rank, Integer> matchCounts, double priceAmount) {
+        this.matchCounts = matchCounts;
+        this.profitRate = calculateProfitRate(priceAmount);
+    }
+
+    public LottoResult from(Map<Rank, Integer> matchCounts, double profitRate) {
+        return new LottoResult(matchCounts, profitRate);
+    }
+
+    public Map<Rank, Integer> getMatchCounts() {
+        return matchCounts;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
+    }
+
+    private double calculateProfitRate(double priceAmount) {
+        long totalPrize = calculateTotalPrize();
+        if (totalPrize == 0) {
+            return 0.0;
+        }
+
+        return (double) totalPrize / priceAmount *100 ;
+    }
+
+    private long calculateTotalPrize() {
+        long totalPrize = matchCounts.entrySet().
+                stream()
+                .mapToLong(this::calculateEachPrize)
+                .sum();
+        return totalPrize;
+    }
+
+    private long calculateEachPrize(Map.Entry<Rank, Integer> entry) {
+        long prize = entry.getKey().getPrize();
+        int count = entry.getValue();
+        return prize * count;
+    }
 }
