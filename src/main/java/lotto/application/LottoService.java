52bc9898a6e3570055433a66bf586cb5773e877b
GB <xorbskim6105@gmail.com>
2025-11-01 21:37:31 +0900

    feat($LottoService) : 로또 계층 추가
    원하는 수만큼 로또를 생성하고 결과를 반환하는 응용 계층 추가

--- src/main/java/lotto/application/LottoService.java
+++ src/main/java/lotto/application/LottoService.java
@@ -1,2 +1,35 @@
-package lotto.application;public class LottoService {
+package lotto.application;
+
+import java.util.List;
+import java.util.Map;
+import lotto.constant.Rank;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.LottoResult;
+import lotto.domain.Lottos;
+import lotto.domain.WinningNumbers;
+
+public class LottoService {
+    private final LottoGenerator generator;
+
+    public LottoService(LottoGenerator generator) {
+        this.generator = generator;
+    }
+
+    public Lottos purchaseLottos(int price){
+        int purchasedCount = price / 1000;
+        Lottos customerLottos = Lottos.createEmpty();
+        for (int i = 0; i < purchasedCount; i++) {
+            List<Integer> numbers = generator.generate();
+            Lotto lotto = new Lotto(numbers);
+            customerLottos = customerLottos.add(lotto);
+        }
+        return customerLottos;
+    }
+
+    public LottoResult checkLottos(Lottos lottos, WinningNumbers winningNumbers, int price){
+        Map<Rank, Integer> matchCount = lottos.match(winningNumbers);
+        LottoResult result = new LottoResult(matchCount, price);
+        return result;
+    }
 }
