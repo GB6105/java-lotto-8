33db9c91022f99526eb7250c5b8354497ef73d1e
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-11-03 18:09:32 +0900

    refactor($Service) : 패키지명 변경
    계층 구조 변경에 따른 패키지명 변경
    application -> service

--- src/main/java/lotto/service/LottoGame.java
+++ src/main/java/lotto/service/LottoGame.java
@@ -1,4 +1,4 @@
-package lotto.application;
+package lotto.service;
 
 import java.util.List;
 import lotto.domain.LottoResult;
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,4 @@
-package lotto.application;
+package lotto.service;
 
 import java.util.List;
 import java.util.Map;
