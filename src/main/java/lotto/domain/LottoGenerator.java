8911a182223d362e9647f13048fa4fa5c2f6f0e3
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-11-03 18:05:15 +0900

    fix($LottoGenerator) : 로또 생성기 인터페이스 화
    lottoGenerator -> 인터페이스 화
    NumberGenerator -> 구현
    구조로 변경

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,14 +1,9 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-public class LottoGenerator {
-    private static final int RANDOM_NUMBER_START = 1;
-    private static final int RANDOM_NUMBER_END = 45;
-    private static final int RANDOM_NUMBER_COUNT = 6;
-
-    public List<Integer> generate(){
-        return Randoms.pickUniqueNumbersInRange(RANDOM_NUMBER_START,RANDOM_NUMBER_END,RANDOM_NUMBER_COUNT);
-    }
+// 위치가 domain인 것 안좋음
+// -> infrastructure로
+public interface LottoGenerator {
+    List<Integer> generate();
 }
--- src/main/java/lotto/infrastructure/NumberGenerator.java
+++ src/main/java/lotto/infrastructure/NumberGenerator.java
@@ -1,2 +1,16 @@
-package lotto.infrastructure;public class NumberGenerator {
+package lotto.infrastructure;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.domain.LottoGenerator;
+
+public class NumberGenerator implements LottoGenerator {
+    private static final int RANDOM_NUMBER_START = 1;
+    private static final int RANDOM_NUMBER_END = 45;
+    private static final int RANDOM_NUMBER_COUNT = 6;
+
+    @Override
+    public List<Integer> generate(){
+        return Randoms.pickUniqueNumbersInRange(RANDOM_NUMBER_START,RANDOM_NUMBER_END,RANDOM_NUMBER_COUNT);
+    }
 }
